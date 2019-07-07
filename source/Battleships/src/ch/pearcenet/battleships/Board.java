86fbb0eda546037c67b254c884c2a002dfb9dc9c
sam <sam@sam>
2019-07-07 21:13:57 +0200

    Inital Commit.

---
+++ source/Battleships/src/ch/pearcenet/battleships/Board.java
@@ -0,0 +1,225 @@
+package ch.pearcenet.battleships;
+
+public class Board {
+	
+	//Standard battleships ship set
+	public static final int[] STNDR_SHIPS = {5, 4, 3, 3, 2};
+	public static final int[] SMALL_SHIPS = {4, 3, 2, 2};
+	
+	//Board data
+	private char[][] ships;
+	private char[][] guess;
+	private int numShips;
+	private int[] shipSet;
+	
+	//Constructor
+	public Board(int size, int[] shipSet) {
+		ships = new char[size][size];
+		guess = new char[size][size];
+		this.shipSet = shipSet.clone();
+		
+		//Initialize grid as "empty"
+		for (int y=0; y<size; y++) {
+			for (int x=0; x<size; x++) {
+				ships[y][x] = ' ';
+				guess[y][x] = ' ';
+			}
+		}
+		
+		//Set numShips to the total number of ship squares
+		numShips = 0;
+		for (int shipSize: shipSet) {
+			numShips += shipSize;
+		}
+	}
+	
+	//Returns the number of ship-squares this player has
+	public int getNumShips() {
+		return numShips;
+	}
+	
+	//Performs a full attack from this board to a victim
+	public void attack(Board victim) {
+		
+		//display the attacker's board and ask for the target
+		legend();
+		this.drawBoard();
+		System.out.println("Enter your guess: (e.g. A1)");
+		
+		//Verify input format
+		boolean isValid = false;
+		String guess = "";
+		while (!isValid) {
+			isValid = true;
+			System.out.print("> ");
+			guess = UI.input.nextLine();
+			
+			//verification
+			if (guess.length() > 2 || 
+				guess.toUpperCase().charAt(0) < 'A' ||
+				guess.toUpperCase().charAt(0) > 'Z' ||
+				guess.charAt(1) < '1' ||
+				guess.charAt(1) > '9'){
+				
+				System.out.println("Please only enter valid guesses. (e.g. A1)");
+				isValid = false;
+			}
+		}
+		
+		//Check that position on the victims board
+		int x = guess.toUpperCase().charAt(0) - 'A' + 1;
+		int y = guess.charAt(1) - '1' + 1;
+		
+		if (victim.ships[y][x] == 'B') {
+			System.out.println("Hit!");
+			victim.ships[y][x] = 'X';
+			this.guess[y][x] = 'X';
+			victim.numShips--;
+		} else {
+			System.out.println("Miss.");
+			victim.ships[y][x] = 'O';
+			this.guess[y][x] = 'O';
+		}
+		
+	}
+	
+	//Fills the ships grid with the set of ships in random positions
+	public void setRandomLayout() {
+		for (int shipSize: shipSet) {
+			
+			//keep trying positions to add the ship until it fits
+			boolean fits = false;
+			int direction = 0;
+			int x = 0;
+			int y = 0;
+			
+			while (!fits) {
+				fits = true;
+				direction = randInt(4);
+				x = randInt(ships.length);
+				y = randInt(ships.length);
+				
+				//Check if the ship would fit here
+				for (int i=0; i<shipSize; i++) {
+					int tempY = y;
+					int tempX = x;
+					switch (direction) {
+						case 1:
+							tempY = y - i;
+							break;
+							
+						case 2:
+							tempX = x + i;
+							break;
+							
+						case 3:
+							tempY = y + i;
+							break;
+						
+						case 4:
+							tempX = x - i;
+							break;
+					}
+					
+					if (tempX < 1 || tempY < 1 || tempX >= ships.length || tempY >= ships.length) {
+						fits = false;
+					} else if (ships[tempY][tempX] != ' ') {
+						fits = false;
+					}
+					
+				}
+			}
+			
+			//When the ship fits add it to the grid
+			for (int i=0; i<shipSize; i++) {
+				int tempX = x;
+				int tempY = y;
+				//Get next coord
+				switch (direction) {
+					case 1:
+						tempY = y - i;
+						break;
+						
+					case 2:
+						tempX = x + i;
+						break;
+						
+					case 3:
+						tempY = y + i;
+						break;
+					
+					case 4:
+						tempX = x - i;
+						break;
+				}
+				
+				//Add to grid
+				ships[tempY][tempX] = 'B';
+			}
+			
+		}
+	}
+	
+	//Method to display the legend
+	public static void legend() {
+		System.out.println("\n"
+				+ " ~ Legend: ~\n"
+				+ "------------\n"
+				+ " BB = Battleship\n"
+				+ " XX = Hit\n"
+				+ " OO = Miss\n"
+				+ "\n");
+	}
+	
+	//Method to display current board on the screen
+	public void drawBoard() {
+		
+		//Display our ships
+		System.out.println("Our ships:");
+		drawMatrix(ships);
+		System.out.println("\nOur guesses:");
+		drawMatrix(guess);
+		
+	}
+	
+	//Method to draw a single char matrix to the screen
+	private static void drawMatrix(char[][] mat) {
+		//printing horizontal axis
+		System.out.print("  ");
+		char letter = 'A';
+		for (int i=0; i<mat.length; i++) {
+			System.out.print(letter++ + " ");
+		}
+		
+		//Top border
+		System.out.print("\n +");
+		for (int i=0; i<mat.length; i++) {
+			System.out.print("--");
+		}
+		System.out.println("+");
+		
+		//Main matrix data
+		for (int y=0; y<mat.length; y++) {
+			System.out.print((y + 1) + "|");
+			for (int x=0; x<mat.length; x++) {
+				System.out.print("" + mat[y][x] + mat[y][x]);
+			}
+			System.out.println("|");
+		}
+		
+		//Bottom border
+		System.out.print(" +");
+		for (int i=0; i<mat.length; i++) {
+			System.out.print("--");
+		}
+		System.out.println("+");
+	}
+	
+	//Generates a random integer within a range
+	private static int randInt(int max) {
+		double rand = Math.random();
+		rand = rand * max + 1;
+		return (int) rand;
+	}
+	
+}
---
+++ source/Battleships/src/ch/pearcenet/battleships/UI.java
@@ -0,0 +1,199 @@
+package ch.pearcenet.battleships;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class UI {
+	
+	//Current version
+	private static final String VERSION = "1.0.1";
+	
+	//Input scanner
+	protected static Scanner input = new Scanner(System.in);
+	
+	//Player boards
+	private static Board board1;
+	private static Board board2;
+	
+	//Main
+	public static void main(String[] args) {
+		
+		//Main menu
+		boolean isRunning = true;
+		while (isRunning) {
+			switch(printMenu()) {
+				//Start the game with 5x5 boards
+				case 1:
+					initBoards(5);
+					break;
+				
+				//Start the game with 9x9 boards
+				case 2:
+					initBoards(9);
+					break;
+				
+				//Exit the program
+				case 3:
+					System.out.println("Thank you for playing!");
+					isRunning = false;
+					System.exit(0);
+					break;
+			}
+			
+			//Start of the actual game logic
+			cls();
+			int winner = 0;
+			int round = 0;
+			long startTime = System.currentTimeMillis();
+			long endTime = 0;
+			
+			while (winner == 0) {
+				round++;
+				
+				//Player 1's turn
+				System.out.println("It is Player 1's turn.\n"
+						+ "Player 1, press enter when ready...");
+				input.nextLine();
+				cls();
+				
+				board1.attack(board2);
+				
+				//Check for winners
+				if (board2.getNumShips() <= 0) {
+					endTime = System.currentTimeMillis();
+					winner = 1;
+					continue;
+				}
+				
+				//Player 2's turn
+				System.out.println("It is Player 2's turn.\n"
+						+ "Player 2, press enter when ready...");
+				input.nextLine();
+				cls();
+				
+				board2.attack(board1);
+				
+				//Check for winners
+				if (board1.getNumShips() <= 0) {
+					endTime = System.currentTimeMillis();
+					winner = 2;
+					continue;
+				}
+				
+			}
+			
+			//Announce winner and display stats
+			cls();
+			System.out.println("                                                                                        \n" + 
+					"  ,- _~.                                 ,        ,,         ,                       /\\ \n" + 
+					" (' /|                 _           _    ||        ||   _    ||   '                   \\/ \n" + 
+					"((  ||    /'\\\\ \\\\/\\\\  / \\\\ ,._-_  < \\, =||= \\\\ \\\\ ||  < \\, =||= \\\\  /'\\\\ \\\\/\\\\  _-_, }{ \n" + 
+					"((  ||   || || || || || ||  ||    /-||  ||  || || ||  /-||  ||  || || || || || ||_.  \\/ \n" + 
+					" ( / |   || || || || || ||  ||   (( ||  ||  || || || (( ||  ||  || || || || ||  ~ ||    \n" + 
+					"  -____- \\\\,/  \\\\ \\\\ \\\\_-|  \\\\,   \\/\\\\  \\\\, \\\\/\\\\ \\\\  \\/\\\\  \\\\, \\\\ \\\\,/  \\\\ \\\\ ,-_-  <> \n" + 
+					"                      /  \\                                                              \n" + 
+					"                     '----`                                                             ");
+			System.out.println("Player " + winner + ", you win!\n");
+			
+			long timeTaken = endTime - startTime;
+			String formattedTime = new SimpleDateFormat("HH:mm:ss").format(new Date(timeTaken));
+			
+			System.out.println("The game lasted " + round + "rounds and took a total time of: " + formattedTime);
+			System.out.println("Press enter to return to the main menu...");
+			input.nextLine();
+			cls();
+		}
+	}
+	
+	//Simply sets up both game boards for any size
+	private static void initBoards(int size) {
+		
+		//Use a custom shipset for different sizes
+		int[] shipSet = Board.STNDR_SHIPS;
+		switch (size) {
+			case 5:
+				shipSet = Board.SMALL_SHIPS;
+				break;
+				
+			case 9:
+				shipSet = Board.STNDR_SHIPS;
+				break;
+			
+			//Future feature: Make any size possible with automatic ship sets
+			default:
+				shipSet = Board.STNDR_SHIPS;
+				break;
+		}
+		
+		board1 = new Board(size, shipSet);
+		board2 = new Board(size, shipSet);
+		System.out.println("Randomizing game boards...");
+		
+		board1.setRandomLayout();
+		board2.setRandomLayout();
+		System.out.println("Done.");
+	}
+	
+	//Displays the main menu and returns result
+	private static int printMenu() {
+		
+		//ASCII art banner
+		System.out.println("                                                            \n" + 
+				"_-_ _,,           ,    ,  ,,             ,,                   \n" + 
+				"   -/  )    _    ||   ||  ||             ||     '             \n" + 
+				"  ~||_<    < \\, =||= =||= ||  _-_   _-_, ||/\\\\ \\\\ -_-_   _-_, \n" + 
+				"   || \\\\   /-||  ||   ||  || || \\\\ ||_.  || || || || \\\\ ||_.  \n" + 
+				"   ,/--|| (( ||  ||   ||  || ||/    ~ || || || || || ||  ~ || \n" + 
+				"  _--_-'   \\/\\\\  \\\\,  \\\\, \\\\ \\\\,/  ,-_-  \\\\ |/ \\\\ ||-'  ,-_-  \n" + 
+				" (                                         _/     |/          \n" + 
+				"                                                  '           ");
+		
+		//General info
+		System.out.println("\nBattleships v" + VERSION +
+							"\n by Samuel Pearce");
+		
+		//Menu options
+		System.out.println("\n"
+				+ "\nPlease choose:"
+				+ "\n [1] Play 5x5"
+				+ "\n [2] Play 9x9"
+				+ "\n [3] Exit game"
+				+ "\n");
+		
+		//Get user input
+		boolean isValid = false;
+		int num = 0;
+	
+		while (!isValid) {
+			isValid = true;
+			System.out.print("> ");
+			String in = input.nextLine();
+			
+			//Verify it's a number
+			try {
+				num = Integer.parseInt(in);
+			} catch (NumberFormatException e) {
+				System.out.println("Please only enter numbers from 1-3");
+				isValid = false;
+				continue;
+			}
+			
+			//Verify it's within range
+			if (num < 1 || num > 3) {
+				System.out.println("Please only enter numbers from 1-3");
+				isValid = false;
+				continue;
+			}
+		}
+		
+		//Return user's input
+		return num;
+	}
+	
+	//Function to "clear" the screen
+	private static void cls() {
+		for (int i=0; i<500; i++) System.out.println("\n");
+	}
+
+}
